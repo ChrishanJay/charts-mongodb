55836af7db6d8325d754feb98e2698e6efc8a99d
Chrishan Jayakody <chrishan@cinnamonit.com>
2020-10-02 20:37:00 +0530

    update chart id

--- server.js
+++ server.js
@@ -3,14 +3,14 @@ const crypto = require('crypto');
 const app = express();
 const port = 3000;
 
-const CHART_EMBEDDING_URL = 'REPLACE_WITH_CHART_URL';
+const CHART_EMBEDDING_URL = 'https://charts.mongodb.com/charts-data-analytics-lqcwa';
 const CHART_TENANT_ID = 'REPLACE_WITH_TENANT_ID';
-const EMBEDDING_KEY = 'REPLACE_WITH_EMBEDDING_KEY';
+const EMBEDDING_KEY = '63254e5f37e24cdf';
 const EXPIRY_IN_SECONDS = 5000;
 
 app.get('/api/verifiedchart/:id', (req, res) => {
     const timestamp = Math.floor(Date.now() / 1000);
-    let payload = `id=${req.params.id}&tenant=${CHART_TENANT_ID}&timestamp=${timestamp}&expires-in=${EXPIRY_IN_SECONDS}`;
+    let payload = `id=${req.params.id}&timestamp=${timestamp}&expires-in=${EXPIRY_IN_SECONDS}`;
     const hmac = crypto.createHmac('sha256', EMBEDDING_KEY);
     hmac.update(payload);
     const signature = hmac.digest('hex');
